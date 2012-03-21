3db0a378343c441bebd0c013ced0332f8f73d3ce
DJ Holt <djholt@server.local>
2012-03-21 12:35:22 -0600

    Add deploy hook

---
+++ lib/big_tuna/hooks/deploy.rb
@@ -0,0 +1,23 @@
+module BigTuna
+  class Hooks::Deploy < Hooks::Base
+
+    NAME = "deploy"
+    CHEF_DIR = "/srv/iditto-chef"
+
+    def build_passed(build, config)
+      BigTuna::Runner.execute build.build_dir, merge_command(build.commit)
+      BigTuna::Runner.execute CHEF_DIR, deploy_command
+    end
+
+    private
+
+    def merge_command(commit)
+      "git clean -d -f && git checkout -- . && git checkout qa && git merge --ff-only #{commit} && git push origin qa"
+    end
+
+    def deploy_command
+      "bundle exec knife ssh 'role:app_server' -x ubuntu -a ec2.public_hostname 'sudo chef-client'"
+    end
+
+  end
+end
