aa666c73932bcf82dd2c79ec437c3e2bc5a7b1dc
DJ Holt <djholt@server.local>
2013-02-11 15:58:36 -0700

    update rails version and add better deploy key configuration

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem "rails", "~> 3.0.11"
+gem "rails", "~> 3.0.19"
 gem "sqlite3-ruby"
 gem "haml"
 gem "delayed_job"
--- lib/big_tuna/hooks/deploy.rb
+++ lib/big_tuna/hooks/deploy.rb
@@ -2,11 +2,14 @@ module BigTuna
   class Hooks::Deploy < Hooks::Base
 
     NAME = "deploy"
-    CHEF_DIR = "/srv/iditto-chef"
+    # CHEF_DIR = "/srv/iditto-chef"
 
     def build_passed(build, config)
       BigTuna::Runner.execute build.build_dir, merge_command(build.commit)
-      BigTuna::Runner.execute CHEF_DIR, deploy_command
+      BigTuna::Runner.execute config['chef_repo_path'], deploy_command(config)
+    rescue BigTuna::Runner::Error => e
+      BigTuna.logger.error(e)
+      BigTuna.logger.error(e.output)
     end
 
     private
@@ -15,8 +18,8 @@ module BigTuna
       "git clean -d -f && git checkout -- . && git checkout qa && git merge --ff-only #{commit} && git push origin qa"
     end
 
-    def deploy_command
-      "bundle exec knife ssh 'chef_environment:qa AND role:app_server' -x ubuntu -a ec2.public_hostname 'sudo chef-client'"
+    def deploy_command(config)
+      "bundle exec knife ssh 'chef_environment:qa AND role:app_server' -x ubuntu -a ec2.public_hostname -i #{config['deploy_key_path']} 'sudo chef-client'"
     end
 
   end
